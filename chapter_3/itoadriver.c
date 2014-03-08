98bef1c2d068f2f9cde95b38f2d4ccb1a3085235
Kirsten Taing <somebody321654@yahoo.com>
2014-03-08 12:08:00 -0500

    Added ex 3-5 to same file as ex 3-4

--- chapter_3/itoadriver.c
+++ chapter_3/itoadriver.c
@@ -2,6 +2,9 @@
    It doesn't work because the largest negative number cannot be represented
    positively. The largest positive number is one smaller in magnitude. */
 
+/* Write function itob(n, s, b) to show the string representation with any
+   base */
+
 #include <limits.h>
 #include <stdio.h>
 
@@ -9,6 +12,7 @@
 
 void itoa(int n, char s[]);
 void reverse(char* str);
+void itob(int n, char s[], int b);
 
 int main() {
 	char str[BUFF];
@@ -17,10 +21,21 @@ int main() {
 	printf("Minimum: %s\n", str);
 	itoa(INT_MAX, str);
 	printf("Maximum: %s\n", str);
+	itob(256, str, 10);
+	printf("256 in decimal: %s\n", str);
+	itob(256, str, 8);
+	printf("256 in octal: %s\n", str);
+	itob(256, str, 16);
+	printf("256 in hex: %s\n", str);
+	itob(256, str, 2);
+	printf("256 in binary: %s\n", str);
+	itob(256, str, 3);
+	printf("256 in ternary: %s\n", str);
 
 	return 0;
 }
 
+// The solution I found online was much more elegant than this.
 void itoa(int n, char s[]) {
 	int i, sign;
 	int largest_neg = 0;
@@ -46,6 +61,50 @@ void itoa(int n, char s[]) {
 	// So there's no way that the LSD is a 9, so this hack should work
 	if (largest_neg)
 		s[0] += 1;
+
+	reverse(s);
+}
+
+void itob(int n, char s[], int b) {
+	int i, sign;
+	int largest_neg = 0;
+
+	if (n == INT_MIN) {
+		n++;
+		largest_neg = 1;
+	}
+
+	if ( (sign=n) < 0)
+		n = -n;
+
+	i = 0;
+	do {
+		s[i++] = n % b + '0';
+	} while ( (n/=b) > 0);
+
+	if (sign < 0)
+		s[i++] = '-';
+
+	s[i] = '\0';
+
+	// Slightly more complicated hack, because it might overflow
+	if (largest_neg) {
+		i = 0;
+		while (s[i] == 9)
+			s[i++] = 0;
+
+		if (s[i] == '-') {
+			s[i++] = 1;
+			s[i++] = '-';
+			s[i++] = '\0';
+		}
+		else if (s[i] == '\0') {
+			s[i++] = 1;
+			s[i++] = '\0';
+		}
+		else
+			s[0] += 1;
+	}
 
 	reverse(s);
 }
