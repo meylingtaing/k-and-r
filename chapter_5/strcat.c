35ef7a44f85061909451dbc9a67b33fabc163dfd
Kirsten Taing <somebody321654@yahoo.com>
2014-03-16 10:51:19 -0400

    Added ex 5-3

---
+++ chapter_5/strcat.c
@@ -0,0 +1,26 @@
+/* Pointer version of strcat */
+
+#include <stdio.h>
+#define BUF 1000
+
+void my_strcat(char *s, char *t);
+
+int main() 
+{
+	char str1[BUF] = "Hello ";
+	char str2[] = "World!";
+
+	my_strcat(str1, str2);
+
+	printf("%s\n", str1);
+}
+
+void my_strcat(char *s, char *t) 
+{
+	// Get to end of s
+	while (*s != 0)
+		s++;
+
+	// Add t
+	while (*s++ = *t++);
+}
