064502b452ebd14e00e50d1a6ed3db5fbd0e54f6
Kirsten Taing <somebody321654@yahoo.com>
2014-04-05 18:59:27 -0400

    Added part of ex 5-11

--- chapter_1/tabtospace.c
+++ chapter_1/tabtospace.c
@@ -1,29 +1,38 @@
 /* Converts input's tabs to space */
 
+/* Allow user to input number of spaces for a tab in command line arg */
+
 #include <stdio.h>
+#include <stdlib.h>
 
 #define TABSTOP 4
 
-int main () {
-    int spaces_needed = TABSTOP;
+int main (int argc, char **argv) {
+    int tabstop = TABSTOP;
+    int spaces_needed;
     int c, i;
+
+    if (argc > 1)
+        tabstop = atoi(argv[1]);
+
+    spaces_needed = tabstop;
 
     while ( (c=getchar()) != EOF) {
 
         if (c == '\t') {
             for (i = 0; i < spaces_needed; i++)
                 putchar(' ');
-            spaces_needed = TABSTOP;
+            spaces_needed = tabstop;
         }
         else {
             putchar(c);
             
             if (c == '\n')
-                spaces_needed = TABSTOP;
+                spaces_needed = tabstop;
             else {
                 spaces_needed--;
                 if (spaces_needed == 0)
-                    spaces_needed = TABSTOP;
+                    spaces_needed = tabstop;
             }
         }
     }
